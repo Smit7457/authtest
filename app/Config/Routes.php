cc7b83101bb5a5da975a4b6dd153350a57819a49
Smit <smit.openmalo@gmail.com>
2024-04-02 14:40:12 +0530

    deploying signup process

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::index');
+
+$routes->get('/signup', 'SignupController::index');
+$routes->match(['get', 'post'], 'signup/store', 'SignupController::store');
---
+++ app/Controllers/SignupController.php
@@ -0,0 +1,48 @@
+<?php 
+namespace App\Controllers;  
+use CodeIgniter\Controller;
+use App\Models\UserModel;
+  
+class SignupController extends Controller
+{
+    public function index()
+    {
+        helper(['form']);
+        $data = [];
+        echo view('signup', $data);
+    }
+  
+    public function store()
+    {
+        helper(['form']);
+        $rules = [
+            'name'          => 'required|min_length[2]|max_length[50]',
+            'email'         => 'required|min_length[4]|max_length[100]|valid_email|is_unique[users.email]',
+            'password'      => 'required|min_length[4]|max_length[50]',
+            'confirmpassword'  => 'matches[password]'
+        ];
+          
+        if($this->validate($rules)){
+            $userModel = new UserModel();
+            $data = [
+                'Name'     => $this->request->getVar('name'),
+                'Email'    => $this->request->getVar('email'),
+                'Mobile'    => $this->request->getVar('mobile'),
+                'Password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT)
+            ];
+            // echo '<pre>'; print_r($data); exit();
+            $userModel->save($data);
+            if ($userModel) {
+                echo 'inserted';
+            }else {
+                echo 'error';
+            }
+            // return redirect()->to('/signin');
+        }else{
+            $data['validation'] = $this->validator;
+            echo view('signup', $data);
+        }
+          
+    }
+  
+}
