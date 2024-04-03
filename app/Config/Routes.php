83fa435b4cf0d3c41e27bccb7502ebe3bbbb4882
Dixit-Chavada-1999 <dixit.openmalo@gmail.com>
2024-04-03 15:01:41 +0530

    pdf file upload and get content using curl

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,10 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-// $routes->get('/', 'Home::index');
+$routes->get('/', 'Home::index');
 
 
-$routes->get('/', 'LoginController::index');
+// $routes->get('/', 'LoginController::index');
 
 $routes->get('/login', 'LoginController::index');
 $routes->post('/checklogin', 'LoginController::checklogin');
