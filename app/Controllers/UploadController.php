8cccb8a2ba69c04770529e1c0b7983951499858d
Dixit-Chavada-1999 <dixit.openmalo@gmail.com>
2024-04-03 15:23:31 +0530

    change name upload folder

--- app/Controllers/UploadController.php
+++ app/Controllers/UploadController.php
@@ -18,8 +18,8 @@ class UploadController extends BaseController
 
         if ($file->isValid() && $file->getExtension() === 'pdf') {
 
-            $file->move(WRITEPATH . '../public/uploads', $file->getName());    
-            $upload_directory = WRITEPATH . '../public/uploads/'. $file->getName();
+            $file->move(WRITEPATH . '../public/Upload', $file->getName());    
+            $upload_directory = WRITEPATH . '../public/Upload/'. $file->getName();
 
             if (file_exists($upload_directory)) {
                 $data['message'] = "File uploaded successfully.";
